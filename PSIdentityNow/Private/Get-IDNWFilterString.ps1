600e6a2b025df67cb76b79e35487624362f919d1
Mathijs de Ruiter <33999622+EUCTechTopics@users.noreply.github.com>
2024-12-12 23:43:14 +0100

    Created modular function to generate filterstring

---
+++ PSIdentityNow/Private/Get-IDNWFilterString.ps1
@@ -0,0 +1,91 @@
+<#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .EXAMPLE
+
+    .PARAMETER Name
+
+    .PARAMETER AsPlainText
+
+    .PARAMETER UseSecretManagement
+
+    .INPUTS
+        None
+
+    .OUTPUTS
+        System.String
+#>
+function Get-IDNWFilterString {
+    [CmdletBinding(
+        SupportsShouldProcess = $False,
+        ConfirmImpact = "None",
+        SupportsPaging = $False,
+        PositionalBinding = $True)
+    ]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-IDNWFilter -Filters $_ })]
+        [Hashtable[]]
+        $Filters
+    )
+
+    # Initiate Empty FilterItems Array
+    $FilterItems = @()
+
+    $Filters | ForEach-Object {
+        # Convert the operator to the IdentityNow API format
+        switch ($_.operator.ToLower()) {
+            'containsall' { $operator = "ca" }
+            'contains' { $operator = "co" }
+            'present' { $operator = "pr" }
+            'startswith' { $operator = "sw" }
+            default { $operator = $_ }
+        }
+        $field = $_.field
+        $value = $_.value
+
+        # Handle operators that don't require a value
+        if ($operator -eq 'pr') {
+            $FilterItems += ('{0} {1}' -f $operator, $field)
+        }
+        elseif ($operator -eq 'isnull') {
+            $FilterItems += ('{0} {1}' -f $field, $operator)
+        }
+        # Handle cases where the value is a collection (e.g., "ca", "in")
+        elseif ($value -is [array]) {
+            $formattedValues = ("""" + ($value -join '","') + """") # Add quotes and commas
+            $FilterItems += ('{0} {1} ({2})' -f $field, $operator, $formattedValues)
+        }
+        # Handle numeric values
+        elseif ($value -is [int]) {
+            $FilterItems += ('{0} {1} {2}' -f $field, $operator, $value)
+        }
+        # Handle boolean values
+        elseif ($value -is [bool]) {
+            $FilterItems += ('{0} {1} {2}' -f $field, $operator, $value.ToString().ToLower())
+        }
+        # Handle string values (add quotes and escape special characters)
+        elseif ($value -is [string]) {
+            $escapedValue = $value.Replace('"', '\"') # Escape double quotes
+            $FilterItems += ('{0} {1} "{2}"' -f $field, $operator, $escapedValue)
+        }
+        # Handle date values (format to ISO 8601)
+        elseif ($value -is [datetime]) {
+            $isoDate = $value.ToString("yyyy-MM-ddTHH:mm:ssZ")
+            $FilterItems += ('{0} {1} {2}' -f $field, $operator, $isoDate)
+        }
+        # Fallback for unknown types
+        else {
+            throw "Unsupported value type for field '$field' and operator '$operator'."
+        }
+    }
+
+    # Join the filter items with "and"
+    $FilterString = ($FilterItems -join ' and ')
+
+    # Return FilterString
+    return $FilterString
+
+}
