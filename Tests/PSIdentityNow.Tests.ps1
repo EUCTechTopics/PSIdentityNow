16d0c4825eb266e83a88e17b07e093bb28b0b875
Mathijs de Ruiter <33999622+EUCTechTopics@users.noreply.github.com>
2024-12-14 12:40:35 +0100

    Added Tests (#11)

---
+++ Tests/PSIdentityNow.Tests.ps1
@@ -0,0 +1,52 @@
+Describe 'Test IDNW Lifecycle' {
+    BeforeAll {
+        Import-Module ../PSIdentityNow/PSIdentityNow.psm1
+        Connect-IDNW
+    }
+
+    It 'Perform CRUD Operations' {
+
+        # Define Segment Name
+        $name = "PESTER TESTS $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
+
+        # Create Segment
+        $data = @{
+            name = $name
+        }
+        $create_result = New-IDNWObject -ObjectType 'segments' -Data $data -Verbose -Debug
+        $create_result | Should -BeOfType "System.Object"
+
+        # Get by ID
+        $result = Get-IDNWObject -ObjectType segments -Id $create_result.id -Verbose -Debug
+        $result | Should -BeOfType "System.Object"
+
+        # Get by Filter
+        $filters = @()
+        $filters += @{
+            field    = "name"
+            operator = "eq"
+            value    = $name
+        }
+        $result = Get-IDNWObject -ObjectType 'segments' -Filters $filters -Verbose -Debug
+        $result | Should -BeOfType "System.Object"
+
+        # Rename
+        $Data = @()
+        $Data += @{
+            op    = "replace"
+            path  = "/name"
+            value = $name + " RENAMED"
+        }
+        $result = Set-IDNWObject -ObjectType 'segments' -Id $create_result.id -Data $Data -Verbose -Debug
+        $result | Should -BeOfType "System.Object"
+
+        # Remove
+        $result = Remove-IDNWObject -ObjectType 'segments' -Id $create_result.id -Confirm:$false -Verbose -Debug
+        $result | Should -Be $null
+
+    }
+
+    AfterAll {
+        Disconnect-IDNW
+    }
+}
