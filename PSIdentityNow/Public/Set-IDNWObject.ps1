cf5c57ae21a28122616f7a108e4cbfd85513369d
Deepesh Sachdeva <197597559+deepeshsachdeva@users.noreply.github.com>
2025-02-04 13:54:32 +0100

    updated to set the values for entitlements

--- PSIdentityNow/Public/Set-IDNWObject.ps1
+++ PSIdentityNow/Public/Set-IDNWObject.ps1
@@ -36,7 +36,7 @@ function Set-IDNWObject {
     ]
     param (
         [Parameter(Mandatory = $True)]
-        [ValidateSet("access-profiles", "roles", "public-identities", "requestable-objects", "segments")]
+        [ValidateSet("access-profiles", "entitlements", "roles", "public-identities", "requestable-objects", "segments")]
         [String]
         $ObjectType,
 
