c18c96a1442baa07800598464ec12414813361a2
Mathijs de Ruiter <33999622+EUCTechTopics@users.noreply.github.com>
2025-02-27 16:42:23 +0100

    debug 3

--- PSIdentityNow/Public/Connect-IDNW.ps1
+++ PSIdentityNow/Public/Connect-IDNW.ps1
@@ -73,7 +73,14 @@ function Connect-IDNW {
     $script:IDNWEnv = Get-IDNWEnvironment @Parameters
 
     # Concatenate full version string with prerelease label if present
-    # $PrereleaseLabel = $MyInvocation.MyCommand.Module.PrivateData.PSData['Prerelease']
+    if ($MyInvocation.MyCommand.Module -and 
+        $MyInvocation.MyCommand.Module.PrivateData -and 
+        $MyInvocation.MyCommand.Module.PrivateData.PSData) {
+        $PrereleaseLabel = $MyInvocation.MyCommand.Module.PrivateData.PSData['Prerelease']
+    }
+    else {
+        $PrereleaseLabel = $null
+    }
     $ModuleVersion = $MyInvocation.MyCommand.Module.Version
     if (-not [string]::isNullOrEmpty($PrereleaseLabel)) {
         $VersionString = ("{0}-{1}" -f $ModuleVersion, $PrereleaseLabel)
@@ -95,6 +102,7 @@ Org:                  $($script:IDNWEnv.SessionTokenDetails.org)
 Authorities:          $($script:IDNWEnv.SessionTokenDetails.Authorities -join ', ')
 Base URL:             $($script:IDNWEnv.BaseAPIURL)
 API Version:          $($script:IDNWEnv.APIVersion)
+Module Version:       $($VersionString)
 Token Expires:        $($script:IDNWEnv.SessionTokenDetails.expiryDateTime)
 
 "@
